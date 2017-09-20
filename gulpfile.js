f08911479c8d18a0dc9d05b74461c9dda86cb6cd
Matt Greenberg <magreenberg@fullsail.edu>
2017-09-19 20:33:24 -0600

    Added gulp tasks: default, scss, browser-sync, scripts, and watch

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+// Const Definitions
+const GULP = require('gulp');
+const SASS = require('gulp-sass');
+const BROWSERSYNC = require('browser-sync').create();
+const POSTCSS = require('gulp-postcss');
+const AUTOPREFIXER = require('autoprefixer');
+const CONCAT = require('gulp-concat');
+const RENAME = require('gulp-rename');
+const UGLIFY = require('gulp-uglify');
+
+// Default Task
+GULP.task('default', ['watch']);
+
+// Process SASS into CSS
+GULP.task('scss', ()=>{
+
+	var processor = [
+		AUTOPREFIXER({ browsers: ['last 2 versions'] })
+	];
+	return GULP.src('./src/scss/*.scss')
+	.pipe(SASS())
+	.pipe(POSTCSS(processor))
+	.pipe(GULP.dest('./dist/css'))
+	.pipe(BROWSERSYNC.reload({
+		stream: true
+	}));
+
+});
+
+// Launch BrowserSync
+GULP.task('browser-sync', ()=>{
+	BROWSERSYNC.init({
+		server: {
+			baseDir: './'
+		}
+	});
+});
+
+// Concat and Minify Javascript
+GULP.task('scripts', ()=>{
+	return GULP.src('./src/js/*.js')
+	.pipe(CONCAT('scripts.js'))
+	.pipe(GULP.dest('./dist/js'))
+	.pipe(RENAME('scripts.min.js'))
+	.pipe(UGLIFY())
+	.pipe(GULP.dest('./dist/js'));
+});
+
+// Watch SCSS for changes
+GULP.task('watch', ['scripts', 'browser-sync', 'scss'], function(){
+	GULP.watch('./scss/**/*.scss', ['scss']);
+});
+	  
